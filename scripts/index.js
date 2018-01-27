aac9835c1a83a03bebc7cf35e1d3148fb79f53f1
EnshaednHiker <nbingham09@gmail.com>
2018-01-26 20:24:48 -0600

    navbar navigating smoothly and styled

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,96 @@ Polished, Compelling Design and Feel The overall look and feel should be neat, c
 
 Responsive, mobile first Will the hiring manager considering your application view your site on a phone or a desktop, or both? Who knows! So make sure your site is responsive and looks good on any screen size. Optimize for mobile first, and optionally progressively enhance for larger screens.
 
-HTML, CSS, jQuery only After learning React, you may be tempted to use it on your portfolio, but that's overkill. Keep the tech simple.
+HTML, CSS, jQuery only. After learning React, you may be tempted to use it on your portfolio, but that's overkill. Keep the tech simple.
 
 Titles, short descriptions, links, and screenshots for each app you showcase, you should include the app name, a link to the live app and repo, a short description, and a compelling screenshot. You'll recall that for the Node capstone, we did not require you to create a high quality front end. If your screenshot for this app is going to be less than stellar, you can instead provide a stock photo that's thematically relevant.
 
-Only code from scratch projects Apps like fizzbuzz and the shopping list do not belong in your portfolio. Most likely, you'll want to include your API hack, your Node capstone, and your full stack capstone. If you've done other high quality projects outside of the course (for instance, maybe you've done a landing page for a small business as a freelance gig), you may want to include them as well, but make sure you're confident that they are as polished as the other projects.
+Only code from scratch projects. Apps like fizzbuzz and the shopping list do not belong in your portfolio. Most likely, you'll want to include your API hack, your Node capstone, and your full stack capstone. If you've done other high quality projects outside of the course (for instance, maybe you've done a landing page for a small business as a freelance gig), you may want to include them as well, but make sure you're confident that they are as polished as the other projects.
 
-Contact info At a minimum, you should include your email address and a link to your GitHub profile. Some students like to add a contact form. That's fine to do, but you should still include your email address.
+Contact info. At a minimum, you should include your email address and a link to your GitHub profile. Some students like to add a contact form. That's fine to do, but you should still include your email address.
 
-No errors in the console Hiring managers usually have a ton of applicants, which means they're initially looking for quick reasons to dismiss you. If they open up Dev Tools on your portfolio site and see errors, that may be enough to pass you over. So make sure your final site doesn't have any errors.
+No errors in the console. Hiring managers usually have a ton of applicants, which means they're initially looking for quick reasons to dismiss you. If they open up Dev Tools on your portfolio site and see errors, that may be enough to pass you over. So make sure your final site doesn't have any errors.
 
-Reasonable load time We'll discuss how to achieve this, but the page should load quickly (i.e., 2 seconds or less).
+Reasonable load time. We'll discuss how to achieve this, but the page should load quickly (i.e., 2 seconds or less).
 */
 
-// causeRepaintsOn = $("h1, h2, h3, p");
 
-// $(window).resize(function() {
-//   causeRepaintsOn.css("z-index", 1);
-// });
+/*******************Navbar Event Listeners********************/
+
+function checkCheckbox(){
+    if($( "input:checked" ).length===1){
+        $(".luxbar-checkbox").click();
+    }
+}
+function handleNavClass(e){
+    $('.nav-link').removeClass('nav-link-active');
+    $(e.target).addClass('nav-link-active');
+}
+
+$('#about-me-link').click(function(e){
+    checkCheckbox()
+    handleNavClass(e);
+    document.querySelector('#about-me').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+});
+
+$('#portfolio-link').click(function(e){
+    checkCheckbox();
+    handleNavClass(e);
+    document.querySelector('#portfolio').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+});
+$('#contact-link').click(function(e){
+    checkCheckbox();
+    handleNavClass(e);
+    document.querySelector('#contact').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+});
+$('#brand').click(function(){
+    checkCheckbox();
+    $('.nav-link').removeClass('nav-link-active');
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+//duplicate this code here and below so that this runs on page load and on scroll
+// var element = document.getElementById("background-image");
+// var navElement = document.getElementById("luxbar-div");
+// var height = element.scrollHeight;
+// var navHeight = navElement.scrollHeight;
+// if (height - navHeight < window.scrollY){
+//     $('#luxbar-div').addClass('navbar-background-change');
+// }
+// else{
+//     $('#luxbar-div').removeClass('navbar-background-change');
+// }
+
+$(document).scroll(function(){
+    var element = document.getElementById("background-image");
+    var navElement = document.getElementById("navbar");
+    var height = element.scrollHeight;
+    var navHeight = navElement.scrollHeight;
+    if (height < window.scrollY){
+        $('#luxbar-div').addClass('navbar-background-change');
+    }
+    else{
+        $('#luxbar-div').removeClass('navbar-background-change');
+    }
+});
+
+function checkedClassHandling(){
+    if($( "input:checked" ).length===0){
+        $('#luxbar-div').addClass("navbar-height");
+    }
+    else {
+        $('#luxbar-div').removeClass("navbar-height");
+    }
+}
+checkedClassHandling();
+$(".luxbar-checkbox").click(function(){
+    checkedClassHandling();
+});
+
